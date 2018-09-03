effa374eb47807741afae789a48602a00d93ef78
Alexander <wondroussquirrel@gmail.com>
2018-09-03 22:12:06 +0300

    добавлено: blade шаблонизатор

---
+++ resources/views/admin/layouts/main.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('admin.layouts.header')
+    <title>Песочница | @yield('title')</title>
+</head>
+<body>
+    @section('content')
+        @show
+</body>
+</html>
--- resources/views/admin/post_list.blade.php
+++ resources/views/admin/post_list.blade.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Post</h1>
-    @foreach ($posts as $post) 
-        <p>
-            $post->title
-        </p>
-    @endforeach
+@extends('admin.layouts.main')
 
-    <a href="{{ route('post.create') }}">Add New</a>
-</body>
-</html>
+@section('title', 'Список постов')
+
+@section('content')
+    <h1>Post List</h1>
+@endsection
